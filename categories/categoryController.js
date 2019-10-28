06bced12057b970b8c7bf3ac4008c62e081d6414
Marcelo Santos <rocha.mer21@gmail.com>
2019-10-28 19:42:08 -0300

    criado as tabelas de listagem das categorias

--- categories/categoryController.js
+++ categories/categoryController.js
@@ -1,28 +1,30 @@
-const express = require("express");
-const router = express.Router();
-const Category = require("./Category");
-const slugify = require("slugify");
+const express = require("express")
+const router = express.Router()
+const Category = require("./Category")
+const slugify = require("slugify")
 
 router.get("/admin/categories/new", (req, res) => {
-  res.render("admin/categories/new");
-});
+  res.render("admin/categories/new")
+})
 
 router.post("/categories/save", (req, res) => {
-  let title = req.body.title;
+  let title = req.body.title
   if (title != undefined) {
     Category.create({
       title,
       slug: slugify(title)
     }).then(() => {
-      res.redirect("/");
-    });
+      res.redirect("/")
+    })
   } else {
-    res.redirect("/admin/categories/new");
+    res.redirect("/admin/categories/new")
   }
-});
+})
 
 router.get("/admin/categories", (req, res) => {
-  res.render("admin/categories/index");
-});
+  Category.findAll().then(categories => {
+    res.render("admin/categories/index", { categories })
+  })
+})
 
-module.exports = router;
+module.exports = router
