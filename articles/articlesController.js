f7228e5fba7180f54c18aa735d7c639e967ec094
Marcelo Santos <rocha.mer21@gmail.com>
2019-11-04 11:59:36 -0300

    criado a parte do novo artigo e add o tinymce editor

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -1,8 +1,16 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
+const Category = require("../categories/Category")
 
-router.get('/articles', (req, res) => {
-  res.send('Rota de Artigos')
+router.get("/articles", (req, res) => {
+  res.send("Rota de Artigos")
+})
+
+router.get("/admin/articles/new", (req, res) => {
+  Category.findAll().then(categories => {
+    res.render("admin/articles/new", { categories: categories })
+  })
+  
 })
 
 module.exports = router
--- categories/categoryController.js
+++ categories/categoryController.js
@@ -14,7 +14,7 @@ router.post("/categories/save", (req, res) => {
       title,
       slug: slugify(title)
     }).then(() => {
-      res.redirect("/")
+      res.redirect("/admin/categories")
     })
   } else {
     res.redirect("/admin/categories/new")
@@ -46,6 +46,42 @@ router.post("/categories/delete", (req, res) => {
     // Null
     res.redirect("/admin/categories")
   }
+})
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+  let id = req.params.id
+
+  if (isNaN(id)) {
+    res.redirect("/admin/categories")
+  }
+
+  Category.findByPk(id)
+    .then(category => {
+      if (category != undefined) {
+        res.render("admin/categories/edit", { category: category })
+      } else {
+        res.redirect("/admin/categories")
+      }
+    })
+    .catch(erro => {
+      res.redirect("/admin/categories")
+    })
+})
+
+router.post("/categories/update", (req, res) => {
+  let id = req.body.id
+  let title = req.body.title
+
+  Category.update(
+    { title: title, slug: slugify(title) },
+    {
+      where: {
+        id: id
+      }
+    }
+  ).then(() => {
+    res.redirect("/admin/categories")
+  })
 })
 
 module.exports = router
