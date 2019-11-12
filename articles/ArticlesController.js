75bb4616d8d85aaae82f80b30de9403d525bb735
Marcelo Santos <rocha.21@gmail.com>
2019-11-12 09:18:37 -0300

    criado a pagina de criacao de artigo

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -4,8 +4,12 @@ const Category = require("../categories/Category");
 const Article = require("./Article");
 const slugify = require("slugify");
 
-router.get("/articles", (req, res) => {
-  res.send("Rota de Artigos");
+router.get("/admin/articles", (req, res) => {
+  Article.findAll({
+    include: [{ model: Category }]
+  }).then(articles => {
+    res.render("admin/articles/index", { articles: articles });
+  });
 });
 
 router.get("/admin/articles/new", (req, res) => {
@@ -15,17 +19,39 @@ router.get("/admin/articles/new", (req, res) => {
 });
 
 router.post("/articles/save", (req, res) => {
-  let title = req.body.title
-  let body = req.body.body
-  let category = req.body.category
+  let title = req.body.title;
+  let body = req.body.body;
+  let category = req.body.category;
 
   Article.create({
     title: title,
     slug: slugify(title),
     body: body,
     categoryId: category
-  })
-
+  }).then(() => {
+    res.redirect("/admin/articles");
+  });
 });
+
+router.post("/articles/delete", (req, res) => {
+  let id = req.body.id
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      Article.destroy({
+        where: {
+          id: id
+        }
+      }).then(() => {
+        res.redirect("/admin/articles")
+      })
+    } else {
+      // Se nao for numero
+      res.redirect("/admin/articles")
+    }
+  } else {
+    // Null
+    res.redirect("/admin/articles")
+  }
+})
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 
 const connection = new Sequelize("guiapress", "marcelo", "654321", {
   host: "localhost",
-  dialect: "mariadb"
+  dialect: "mariadb",
+  timezone: "-03:00"
 });
 
 module.exports = connection;
