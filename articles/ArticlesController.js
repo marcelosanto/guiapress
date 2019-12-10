daa1a5c9d759e238fc22113c4601238af30ba803
Marcelo Santos <rocha.21@gmail.com>
2019-12-10 10:09:28 -0300

    paginação completa

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -97,12 +97,13 @@ router.get("/articles/page/:num", (req, res) => {
   if (isNaN(page) || page === 1) {
     offset = 0
   } else {
-    offset = parseInt(page) * 4
+    offset = (parseInt(page) - 1) * 4
   }
 
   Article.findAndCountAll({
     limit: 4,
-    offset: offset
+    offset: offset,
+    order: [["id", "DESC"]]
   }).then(articles => {
     let next
     if (offset + 4 >= articles.count) {
@@ -112,12 +113,13 @@ router.get("/articles/page/:num", (req, res) => {
     }
 
     let result = {
+      page: parseInt(page),
       next,
       articles
     }
 
     Category.findAll().then(categories => {
-      res.render('admin/articles/page', { result, categories })
+      res.render("admin/articles/page", { result, categories })
     })
   })
 })
