89137975521a4cc52028158faca5c798280c095c
Marcelo Santos <rocha.21@gmail.com>
2020-01-22 14:59:45 -0300

    criado a view de cadastro

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const express = require("express")
+const express = require('express')
 const app = express()
-const bodyParser = require("body-parser")
-const connection = require("./database/database")
+const bodyParser = require('body-parser')
+const connection = require('./database/database')
 
-const categoriesController = require("./categories/CategoryController")
-const articlesController = require("./articles/ArticlesController")
+const categoriesController = require('./categories/CategoryController')
+const articlesController = require('./articles/ArticlesController')
+const usersController = require('./user/UsersController')
 
-const Article = require("./articles/Article")
-const Category = require("./categories/Category")
+const Article = require('./articles/Article')
+const Category = require('./categories/Category')
+const User = require('./user/User')
 
 // View engine
-app.set("view engine", "ejs")
+app.set('view engine', 'ejs')
 
 // Static
-app.use(express.static("public"))
+app.use(express.static('public'))
 
 //Body parser
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -23,66 +25,67 @@ app.use(bodyParser.json())
 connection
   .authenticate()
   .then(() => {
-    console.log("Conexao feita com sucesso!")
+    console.log('Conexao feita com sucesso!')
   })
   .catch(err => console.log(err))
 
-app.use("/", categoriesController)
-app.use("/", articlesController)
+app.use('/', categoriesController)
+app.use('/', articlesController)
+app.use('/', usersController)
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   Article.findAll({
-    order: [["id", "DESC"]],
-    limit: 4
+    order: [['id', 'DESC']],
+    limit: 4,
   }).then(articles => {
     Category.findAll().then(categories => {
-      res.render("index", { articles: articles, categories: categories })
+      res.render('index', { articles: articles, categories: categories })
     })
   })
 })
 
-app.get("/:slug", (req, res) => {
+app.get('/:slug', (req, res) => {
   let slug = req.params.slug
   Article.findOne({
     where: {
-      slug: slug
-    }
+      slug: slug,
+    },
   })
     .then(article => {
       if (article != undefined) {
         Category.findAll().then(categories => {
-          res.render("article", { article: article, categories: categories })
+          res.render('article', { article: article, categories: categories })
         })
       } else {
-        res.redirect("/")
+        res.redirect('/')
       }
     })
-    .catch(err => res.redirect("/"))
+    .catch(err => res.redirect('/'))
 })
 
-app.get("/category/:slug", (req, res) => {
+app.get('/category/:slug', (req, res) => {
   let slug = req.params.slug
   Category.findOne({
     where: {
-      slug: slug
+      slug: slug,
     },
-    include: [{ model: Article }]
+    include: [{ model: Article }],
   })
     .then(category => {
       if (category != undefined) {
         Category.findAll().then(categories => {
-          res.render("index", {
+          res.render('index', {
             articles: category.articles,
-            categories: categories
+            categories: categories,
           })
         })
       } else {
-        res.redirect("/")
+        res.redirect('/')
       }
     })
-    .catch(err => res.redirect("/"))
+    .catch(err => res.redirect('/'))
 })
 
 app.listen(8080, () => {
-  console.log("O servidor esta rodando")
+  console.log('O servidor esta rodando')
 })
