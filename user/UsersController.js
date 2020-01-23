6eccec3d02b13c0d7a599485e0ae1a730f27b2fe
Marcelo Santos <rocha.21@gmail.com>
2020-01-23 11:51:59 -0300

    criado a view para ver os usuarios

--- user/UsersController.js
+++ user/UsersController.js
@@ -4,7 +4,11 @@ const User = require('./User')
 const bcrypt = require('bcryptjs')
 
 router.get('/admin/users', (req, res) => {
-  res.send('Listagem de usuários')
+  User.findAll().then(users => {
+    res.render('admin/users/index', {
+      users,
+    })
+  })
 })
 
 router.get('/admin/users/create', (req, res) => {
@@ -15,11 +19,15 @@ router.post('/users/create', (req, res) => {
   let email = req.body.email
   let password = req.body.password
 
-  User.findOne({ where: { email: email } }).then(user => {
-    if (user === undefined) {
+  User.findOne({ where: { email } }).then(user => {
+    if (email == '' || password == '') {
+      res.redirect('/admin/users/create')
+    }
+
+    if (user == undefined) {
       let salt = bcrypt.genSaltSync(10)
       let hash = bcrypt.hashSync(password, salt)
-
+      console.log(email, hash)
       User.create({
         email,
         password: hash,
@@ -28,6 +36,7 @@ router.post('/users/create', (req, res) => {
           res.redirect('/')
         })
         .catch(err => {
+          console.log(err)
           res.redirect('/')
         })
     } else {
